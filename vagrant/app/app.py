0a2a6fdebd8c39e61acd6a14e7c938e47aaec448
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-05-21 21:32:54 -0800

    Feat: Created RESTful API for getting items
    Added a helper function to create data for dev database and ensure its
    clean

--- vagrant/app/app.py
+++ vagrant/app/app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
+import app_helper
+import models
+from model_pipeline import ItemCatalogPipeline
+
 
 app = Flask(__name__)
 
 app.config.update(dict(
-    DEBUG=True
+    DEBUG=True,
+    PIPELINE=ItemCatalogPipeline()
 ))
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
+@app.route('/j/items', methods=['GET'])
+def get_all_items():
+    session = app.config['PIPELINE'].session()
+    end_session = app.config['PIPELINE'].finalize_session
+    query = session.query(models.Items).all()
+    # end_session(session)
+    return app_helper.create_json(query)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
