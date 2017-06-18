f774dfe43ca09a4c1c2afb7e15b90c7410a7df3c
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-06-18 00:48:10 -0800

    Test: Enzyme&Jest based component tests

--- vagrant/app/static/scripts/app/Components.test.js
+++ vagrant/app/static/scripts/app/Components.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import serializer from 'enzyme-to-json/serializer';
 import Navbar from './Navbar';
 
+expect.addSnapshotSerializer(serializer);
+
 describe('Components', () => {
+    const navbar = shallow(<Navbar />);
     describe('Navbar', () => {
         it('should render', () => {
-            const navbar = shallow(<Navbar/>);
-            expect(navbar.find('Navbar').hasClass('navbar')).toBe(true);
+            expect(navbar.find('nav').hasClass('navbar')).toBe(true);
         });
-
+        it('Should contain brand text', () => {
+            expect(navbar.findWhere(n => n.is('.navbar-brand')).text()).toBe(
+                'Catalog App'
+            );
+        });
+        it('Should match snapshot', () => {
+            expect(navbar).toMatchSnapshot();
+        });
     });
 });
-
