29df61629fff903107ecc6193b9980ae3d5099c8
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-06-18 01:03:50 -0800

    Test: Added Header tests

--- vagrant/app/static/scripts/app/Components.test.js
+++ vagrant/app/static/scripts/app/Components.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import serializer from 'enzyme-to-json/serializer';
 import Navbar from './Navbar';
+import Header from './Header';
 
 expect.addSnapshotSerializer(serializer);
 
 describe('Components', () => {
     describe('Navbar', () => {
         const navbar = shallow(<Navbar />);
-        it('should render', () => {
+        it('Should render', () => {
             expect(navbar.find('nav').hasClass('navbar')).toBe(true);
         });
         it('Should contain brand text', () => {
@@ -18,6 +19,20 @@ describe('Components', () => {
         });
         it('Should match snapshot', () => {
             expect(navbar).toMatchSnapshot();
+        });
+    });
+    describe('Header', () => {
+        const header = shallow(<Header />);
+        it('Should render', () => {
+            expect(header.find('.page-header').type('div')).toBe('div');
+        });
+        it('Should contain Catalog text', () => {
+            expect(header.findWhere(n => n.is('.col-md-4')).text()).toBe(
+                'Catalog App'
+            );
+        });
+        it('Should match snapshot', () => {
+            expect(header).toMatchSnapshot();
         });
     });
 });
