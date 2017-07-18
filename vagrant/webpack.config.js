e8edbc41084134b0701ca7418d754437af3ff7b9
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-07-17 22:17:09 -0800

    Chore: Webpack update and sass support

--- vagrant/webpack.config.js
+++ vagrant/webpack.config.js
@@ -2,46 +2,44 @@ var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var extractPlugin = new ExtractTextPlugin({
-    filename: 'main.css'
+    filename: 'main.css',
 });
-
 
 module.exports = {
     entry: './app/static/scripts/app/app.jsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'app/static/dist'),
-        publicPath: '/app/static/dist'
+        publicPath: '/app/static/dist',
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env'],
+                        },
+                    },
                 ],
                 exclude: /(node_modules)/,
             },
             {
                 test: /\.scss$/,
                 use: extractPlugin.extract({
-                    use: ['css-loader','sass-loader']
-                })
+                    use: ['css-loader', 'sass-loader'],
+                }),
             },
             {
                 test: /\.(woff2?|ttf|eot|svg)$/,
-                loader: 'file-loader'
-            }
-        ]
+                loader: 'file-loader',
+            },
+        ],
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
     },
-    plugins: [
-        extractPlugin
-    ]
+    plugins: [extractPlugin],
 };
