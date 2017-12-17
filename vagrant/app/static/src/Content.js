d563b61c9c6ef12e3d1d76156ae5bfbd49150c8f
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-12-17 11:30:20 -0900

    Feat: Removed empty HTML and fixed formatting

--- vagrant/app/static/src/Content.js
+++ vagrant/app/static/src/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Categories from './Categories';
+import Items from './Items';
 
 const Content = () => (
 
@@ -11,19 +12,19 @@ const Content = () => (
       <div className="row">
         <div className="col s12 m4">
           <div className="icon-block">
-            <h2 className="center light-blue-text" />
+            <br />
             <h5 className="center">Categories</h5>
-
-            <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <br />
+            <Categories />
           </div>
         </div>
 
         <div className="col s12 m8">
           <div className="icon-block">
-            <h2 className="center light-blue-text" />
+            <br />
             <h5 className="center">Latest Items</h5>
-
-            <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <br />
+            <Items />
           </div>
         </div>
       </div>
