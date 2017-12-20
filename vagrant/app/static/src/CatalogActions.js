4eca83ea5d4cd0db79d01d9cbc671845df4cc3cd
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-12-19 23:48:29 -0900

    Feat: Actions and onClick for Categories.js

--- vagrant/app/static/src/CatalogActions.js
+++ vagrant/app/static/src/CatalogActions.js
@@ -1,4 +1,11 @@
-export getCategories = () => ({
-    type: "SET_CATEGORIES",
-    payload: fetch()
-})
+import { fetchItem } from './Api';
+
+export const getCategories = () => ({
+  type: 'GET_CATEGORIES',
+  payload: null,
+});
+
+export const getItem = (id = 1) => ({
+  type: 'GET_ITEM',
+  payload: fetchItem(id),
+});
--- vagrant/app/static/src/Categories.js
+++ vagrant/app/static/src/Categories.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getItem } from './CatalogActions';
 
 const catPlacehodler = { categories: ['Soccer', 'Baseball', 'Basketball', 'Frisbee', 'Snowboarding', 'Rock Climbing', 'Foosball', 'Skating', 'Hockey'] };
 
 const CategorieList = (props) => {
-  const { categories } = props;
+  const { categories } = { categories: ['Soccer', 'Baseball', 'Basketball', 'Frisbee', 'Snowboarding', 'Rock Climbing', 'Foosball', 'Skating', 'Hockey'] };
   let i = 0;
   return (
     <div className="collection">
       {
         categories.map(category => (
-          <a href="#!" key={i++} className="collection-item">{category}</a>
+          <a href="#!" key={i++} onClick={() => { props.item(1); }} className="collection-item">{category}</a>
         ))
     }
     </div>
   );
 };
+const mapDispatchToProps = dispatch => ({
+  item: id => dispatch(getItem(1)),
+});
 CategorieList.defaultProps = {
   categories: [],
 };
@@ -23,4 +28,4 @@ CategorieList.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.string),
 };
 
-export default () => CategorieList(catPlacehodler);
+export default connect(null, mapDispatchToProps)(CategorieList);
